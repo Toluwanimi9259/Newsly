52835a1acf2e0c864dbc88ce4ac49b49b6cc02a9
The Mafia <issactoluwanimi@gmail.com>
2023-01-23 07:19:02 -0800

    1. Connected bottom navigation to the fragments

--- app/src/main/java/com/techafresh/apps/newsly/MainActivity.kt
+++ app/src/main/java/com/techafresh/apps/newsly/MainActivity.kt
@@ -2,10 +2,28 @@ package com.techafresh.apps.newsly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.techafresh.apps.newsly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    lateinit var navHostFragment: NavHostFragment
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNavX.setupWithNavController(navController)
     }
 }
